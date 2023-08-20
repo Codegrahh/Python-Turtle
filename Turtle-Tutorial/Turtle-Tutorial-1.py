72d638af6ae8c340be20d2b334cc25272b8b81ef
Shreyansh Singh <sriyanshdon2005@gmail.com>
2023-08-20 12:50:03 +0530

    new

--- Turtle-Tutorial/Turtle-Tutorial-1.py
+++ Turtle-Tutorial/Turtle-Tutorial-1.py
@@ -1,1 +1,49 @@
 #Turtle Graphics Tutorial Code Part 1
+import turtle
+
+#Making Object
+pen=turtle.Turtle()
+
+#Square Shape
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+
+#Left Function
+pen.left(90)
+pen.forward(100)
+
+#Goto Function
+pen.goto(100,200)
+
+#Home Function
+pen.home()
+
+#Stamp Function
+pen.stamp()
+pen.forward(100)
+#ClearStamp Function
+pen.clearstamps()
+
+#Penup
+pen.penup()
+pen.forward(100)
+#pendown
+pen.pendown()
+pen.forward(100)
+
+#Pensize
+pen.pensize(3)
+pen.forward(100)
+
+#Undo function
+pen.undo()
+
+#Title Function
+turtle.title("Turtle Tutorial")
+
+turtle.done()            #Subscribe To CodeGrahHHHH.
